6066bfdeebe3227a708f2f4b77d39bc8778e70fe
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-09 10:52:53 -0500

    commit tree 4919e0eee715554ab4846b099bf877352b412500

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number > 1
+    factor = number - 1
+    range = (2..factor)
+    a_range = range.to_a
+    # above is all good
+  
+    a_range.all? do |n| 
+      number % n != 0
+    end
+  else
+      false
+  end
+end
